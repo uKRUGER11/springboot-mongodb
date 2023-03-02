18e125db32f4a470c95af47e413cfd90ad7165ab
Gabriel Kruger <krugergabriel27@outlook.com>
2023-03-02 20:24:30 -0300

    Project created

---
+++ src/main/java/com/kruger/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,11 @@
+package com.kruger.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+}
