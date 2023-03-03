95224e789761a953c85db3cd984054db22160e38
Gabriel Kruger <krugergabriel27@outlook.com>
2023-03-03 19:15:36 -0300

    Database installation operation

---
+++ src/main/java/com/kruger/workshopmongo/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.kruger.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.kruger.workshopmongo.domain.User;
+import com.kruger.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+}
