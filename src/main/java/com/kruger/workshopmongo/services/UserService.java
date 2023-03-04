2c17a7c5d0bb497b145307c620d14cb30cc11b81
Gabriel Kruger <krugergabriel27@outlook.com>
2023-03-04 08:38:09 -0300

    User insertion with POST

--- src/main/java/com/kruger/workshopmongo/services/UserService.java
+++ src/main/java/com/kruger/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kruger.workshopmongo.domain.User;
+import com.kruger.workshopmongo.dto.UserDTO;
 import com.kruger.workshopmongo.repository.UserRepository;
 import com.kruger.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
