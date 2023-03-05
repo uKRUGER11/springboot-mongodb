43df9d0b0d88311038cf517c5a55952730a5b192
Gabriel Kruger <krugergabriel27@outlook.com>
2023-03-05 08:38:24 -0300

    Getting a post by ID

---
+++ src/main/java/com/kruger/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.kruger.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kruger.workshopmongo.domain.Post;
+import com.kruger.workshopmongo.repository.PostRepository;
+import com.kruger.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+	}
+}
